add28a9524ee6845195a45cb1eac4369085a57a8
tarahwaters <thbwaters@outlook.com>
2023-10-24 17:33:46 +0000

    Add profile function in python

--- app.py
+++ app.py
@@ -74,6 +74,14 @@ def signin():
     return render_template("signin.html")
 
 
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # retrieve the session user's username from database
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
