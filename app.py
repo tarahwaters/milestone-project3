5a204bd82c671d055c0eeff4db95910887c4b3b6
tarahwaters <thbwaters@outlook.com>
2023-10-25 09:52:04 +0000

    Fix error of missing render_template return for register.html

--- app.py
+++ app.py
@@ -44,7 +44,8 @@ def register():
         # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
-    return redirect(url_for("profile", username=session["user"]))
+        return redirect(url_for("profile", username=session["user"]))
+    return render_template("register.html")
 
 
 @app.route("/signin", methods={"GET", "POST"})
