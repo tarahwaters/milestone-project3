1f56c23d503321d8daf43edfb1bab3973aa7d1ca
tarahwaters <thbwaters@outlook.com>
2024-01-22 11:45:07 +0000

    Set debug to false for deployment

--- app.py
+++ app.py
@@ -391,4 +391,4 @@ def validate_cafe(cafe):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
