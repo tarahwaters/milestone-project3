a3b9a6de9c12209a47b9a54df67fc795b712f152
tarahwaters <thbwaters@outlook.com>
2024-01-22 06:34:12 +0000

    Replace app.py with validated python, no errors

--- app.py
+++ app.py
@@ -34,9 +34,11 @@ def signin_required(f):
 
     return signin_wrap
 
-# Admin required decorator 
+# Admin required decorator
 # code credit from https://www.perplexity.ai/ and
 # https://flask-admin.readthedocs.io/en/latest/introduction/#securing-admin-views
+
+
 def admin_required(f):
     @wraps(f)
     def admin_wrap(*args, **kwargs):
@@ -61,7 +63,8 @@ def search():
     query = request.form.get("query")
     cafes = list(mongo.db.cafes.find({"$text": {"$search": query}}))
     countries = mongo.db.countries.find().sort("country_name", 1)
-    return render_template("cafes.html", cafes=cafes, countries=countries, query=query)
+    return render_template(
+                "cafes.html", cafes=cafes, countries=countries, query=query)
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -101,13 +104,14 @@ def signin():
             if existing_user:
                 # make sure hashed passwords match user input
                 if check_password_hash(
-                    existing_user["password"], request.form.get("password")):
-                        session["user"] = request.form.get("username").lower()
-                        username = existing_user.get("username")
-                        # for admin users
-                        if existing_user.get("username") == "admin":
-                            # set the admin session variable
-                            session["admin"] = True
+                        existing_user["password"], request.form.get(
+                        "password")):
+                    session["user"] = request.form.get("username").lower()
+                    username = existing_user.get("username")
+                    # for admin users
+                    if existing_user.get("username") == "admin":
+                        # set the admin session variable
+                        session["admin"] = True
                         flash("Welcome, {}".format(
                             request.form.get("username")))
                         return redirect(url_for(
@@ -117,14 +121,12 @@ def signin():
                     # passwords do not match
                     flash("Sorry! Incorrect Username and/or Password")
                     return redirect(url_for("signin"))
-                
             else:
                 # username doesn't exist in database
                 flash("Sorry! Incorrect Username and/or Password")
                 return redirect(url_for("signin"))
 
         return render_template("signin.html")
-    
     # user already signed in, then direct to user profile
     return redirect(url_for("profile", username=session["user"]))
 
@@ -135,20 +137,20 @@ def profile(username):
     if "user" in session:
         session_username = session["user"]
         # retrieve the session user's username from database
-        if session_username:     
+        if session_username:
             if session_username == "admin":
                 # retrieves all cafes for admin users
                 cafes = list(mongo.db.cafes.find())
             else:
                 # retrieves only user published cafes for non-admin users
-                cafes = list(mongo.db.cafes.find({"published_by": session_username}))
-        
+                cafes = list(mongo.db.cafes.find(
+                        {"published_by": session_username}))
         # retrieve image data for countries
         countries = list(mongo.db.countries.find())
 
         return render_template(
-            "profile.html", username=session_username,  cafes=cafes, countries=countries)
-    
+            "profile.html", username=session_username,
+            cafes=cafes, countries=countries)
     return redirect(url_for("signin"))
 
 
@@ -187,15 +189,15 @@ def add_cafe():
         else:
             flash(error_msg)
             return redirect(url_for("add_cafe"))
-
-
     countries = mongo.db.countries.find().sort("country_name", 1)
     power_options = mongo.db.power_options.find().sort("power_outlets", 1)
     free_wifi_options = mongo.db.free_wifi_options.find().sort("free_wifi", 1)
-    wifi_speed_options = mongo.db.wifi_speed_options.find().sort("wifi_speed", 1)
+    wifi_speed_options = mongo.db.wifi_speed_options.find().sort(
+            "wifi_speed", 1)
     return render_template(
-        "add_cafe.html", countries=countries, power_options=power_options, 
-        free_wifi_options=free_wifi_options, wifi_speed_options=wifi_speed_options)
+            "add_cafe.html", countries=countries, power_options=power_options,
+            free_wifi_options=free_wifi_options,
+            wifi_speed_options=wifi_speed_options)
 
 
 @app.route("/edit_cafe/<cafe_id>", methods=["GET", "POST"])
@@ -203,7 +205,8 @@ def edit_cafe(cafe_id):
     if request.method == "POST":
         current_date = datetime.now().strftime("%d/%m/%Y")
         if "admin" in session and session["admin"] or (
-            session["user"] == mongo.db.cafes.find_one({"_id": ObjectId(cafe_id)})['published_by']):
+                session["user"] == mongo.db.cafes.find_one(
+                        {"_id": ObjectId(cafe_id)})['published_by']):
             # Edits the cafe for admin user or published owner of the cafe
             mongo.db.cafes.update_one(
                 {"_id": ObjectId(cafe_id)}, {
@@ -212,7 +215,8 @@ def edit_cafe(cafe_id):
                         "city_name": request.form.get("city_name"),
                         "country_name": request.form.get("country_name"),
                         "map_link": request.form.get("map_link"),
-                        "cafe_description": request.form.get("cafe_description"),
+                        "cafe_description": request.form.get(
+                                "cafe_description"),
                         "power_outlets": request.form.get("power_outlets"),
                         "free_wifi": request.form.get("free_wifi"),
                         "wifi_speed": request.form.get("wifi_speed"),
@@ -235,7 +239,6 @@ def edit_cafe(cafe_id):
             }
             flash("Cafe Updated Successfully!")
             return redirect(url_for("get_cafes"))
-            
         else:
             # When user is not an admin or the published owner of the cafe
             flash("You are not authorized to perform this action")
@@ -245,16 +248,20 @@ def edit_cafe(cafe_id):
     countries = mongo.db.countries.find().sort("country_name", 1)
     power_options = mongo.db.power_options.find().sort("power_outlets", 1)
     free_wifi_options = mongo.db.free_wifi_options.find().sort("free_wifi", 1)
-    wifi_speed_options = mongo.db.wifi_speed_options.find().sort("wifi_speed", 1)
+    wifi_speed_options = mongo.db.wifi_speed_options.find().sort(
+        "wifi_speed", 1)
     return render_template(
-        "edit_cafe.html", cafe=cafe, countries=countries, power_options=power_options, 
-        free_wifi_options=free_wifi_options, wifi_speed_options=wifi_speed_options)
+        "edit_cafe.html", cafe=cafe, countries=countries,
+        power_options=power_options,
+        free_wifi_options=free_wifi_options,
+        wifi_speed_options=wifi_speed_options)
 
 
 @app.route("/delete_cafe/<cafe_id>")
 def delete_cafe(cafe_id):
     if "admin" in session and session["admin"] or (
-            session["user"] == mongo.db.cafes.find_one({"_id": ObjectId(cafe_id)})['published_by']):
+            session["user"] == mongo.db.cafes.find_one(
+                    {"_id": ObjectId(cafe_id)})['published_by']):
         # Deletes the cafe for admin user or cafe owner
         mongo.db.cafes.delete_one({"_id": ObjectId(cafe_id)})
         flash("Cafe Deleted Successfully")
@@ -262,7 +269,6 @@ def delete_cafe(cafe_id):
     else:
         flash("You are not authorized to perform this action")
         return redirect(url_for("get_cafes"))
-    
     cafe = mongo.db.cafes.find_one({"_id": ObjectId(cafe_id)})
     return render_template("cafes.html", cafe=cafe)
 
@@ -271,11 +277,13 @@ def delete_cafe(cafe_id):
 def delete_user_cafe(cafe_id):
     cafe = mongo.db.cafes.find_one({"_id": ObjectId(cafe_id)})
     if "admin" in session and session["admin"] or (
-            session["user"] == mongo.db.cafes.find_one({"_id": ObjectId(cafe_id)})['published_by']):
+            session["user"] == mongo.db.cafes.find_one(
+                {"_id": ObjectId(cafe_id)})['published_by']):
         # Deletes the cafe for admin user or cafe owner via the profile page
         mongo.db.cafes.delete_one({"_id": ObjectId(cafe_id)})
         flash("Cafe Deleted Successfully")
-        return redirect(url_for("profile", username=session["user"]))  # Redirect to user's profile
+        return redirect(url_for(
+            "profile", username=session["user"]))  # Redirect to user's profile
     else:
         flash("You are not authorized to perform this action")
         return redirect(url_for("get_cafes"))
@@ -320,7 +328,6 @@ def edit_country(country_id):
         )
         flash("Country Successfully Updated")
         return redirect(url_for("get_countries"))
-        
     country = mongo.db.countries.find_one({"_id": ObjectId(country_id)})
     return render_template("edit_country.html", country=country)
 
@@ -332,8 +339,11 @@ def delete_country(country_id):
     flash("Country Successfully Deleted")
     return redirect(url_for("get_countries"))
 
-# Credit for code adapted from http://my-milestone-project3.herokuapp.com/, 
-# more specifically https://github.com/Dynjashik/MSP3-Movie-collection/blob/master/app.py
+# Credit for code adapted from http://my-milestone-project3.herokuapp.com/,
+# more specifically..
+# https://github.com/Dynjashik/MSP3-Movie-collection/blob/master/app.py
+
+
 def validate_cafe(cafe):
     is_valid = True
     error_msg = []
@@ -341,47 +351,41 @@ def validate_cafe(cafe):
     if len(cafe["cafe_name"]) < 1:
         is_valid = False
         error_msg.append("Cafe name should be at least 1 character long")
-    
     # validate town / city name
     if len(cafe["city_name"]) < 1:
         is_valid = False
-        error_msg.append("Town / city name should be at least 1 character long")
-    
-    #validate country selection
+        error_msg.append(
+                "Town / city name should be at least 1 character long")
+    # validate country selection
     if not cafe["country_name"]:
         is_valid = False
         error_msg.append("Country is not selected")
-    
     # validate google map link
     if not cafe["map_link"]:
         is_valid = False
         error_msg.append("Google Map link must be specified")
-    
     # validate cafe_description
     if len(cafe["cafe_description"]) < 10:
         is_valid = False
-        error_msg.append("Description should be between 10 and 250 chracters long")
-    
-    #validate power outlets
+        error_msg.append(
+            "Description should be between 10 and 250 chracters long")
+    # validate power outlets
     if not cafe["power_outlets"]:
         is_valid = False
         error_msg.append("Option is not selected")
-    
-    #validate free wifi
+    # validate free wifi
     if not cafe["free_wifi"]:
         is_valid = False
         error_msg.append("Option is not selected")
-    
-    #validate wifi speed
+    # validate wifi speed
     if not cafe["wifi_speed"]:
         is_valid = False
         error_msg.append("Option is not selected")
-    
     return is_valid, error_msg
-    
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+            
